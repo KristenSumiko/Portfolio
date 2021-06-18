ef80d5fabefde6f6bd51eea64fce681c9564f605
KristenSumiko <kristensumiko@gmail.com>
2021-06-18 12:48:48 -0400

    Bio Updated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-  
-    $(".scrollTo").on("click", function( e ) {
-  
-        e.preventDefault();
-    
-        $("body, html").animate({ 
-          scrollTop: $( $(this).attr('href') ).offset().top 
-        }, 600);
-    
-      });
-})
+$(document).ready(function () {
+  $(".scrollTo").on("click", function (e) {
+    e.preventDefault();
+
+    $("body, html").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
+      600
+    );
+  });
+});
